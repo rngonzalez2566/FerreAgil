cf51ad4ca44ee20909aca11bb4a0506ef8ef2647
Rodrigo Nicolas Gonzalez <rngonzalez2566@gmail.com>
2022-06-19 09:34:46 -0300

    Form producto

---
+++ UI/Producto/AltaProducto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Producto
+{
+    public partial class AltaProducto : Form
+    {
+        public AltaProducto()
+        {
+            InitializeComponent();
+        }
+
+        private void AltaProducto_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
